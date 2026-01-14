31564d0e54fae6389828980a922d1c626c7f2f94
Pablo Gómez <pablogomez@pablogomez.com>
2026-01-14 22:02:19 +0000

    refactor: nvf v8

--- neovim.nix
+++ neovim.nix
@@ -1,59 +1,267 @@
 { pkgs, ... }:
 {
   vim = {
-    assistant = {
-      chatgpt.enable = true;
-      copilot = {
-        enable = true;
-        cmp.enable = true;
-      };
-    };
-    autocomplete.nvim-cmp.enable = true;
-    binds = {
-      cheatsheet.enable = true;
-      whichKey.enable = true;
-    };
-    comments.comment-nvim.enable = true;
     debugger = {
       nvim-dap = {
         enable = true;
         ui.enable = true;
       };
     };
-    extraPlugins = with pkgs.vimPlugins; {
-      neogit = {
-        package = neogit;
-        setup = "require('neogit').setup()";
+    debugMode = {
+      enable = false;
+      level = 16;
+      logFile = "/tmp/nvim.log";
+    };
+    languages = {
+      enableFormat = true;
+      enableTreesitter = true;
+      enableExtraDiagnostics = true;
+      nix = {
+        enable = true;
+        format = {
+          enable = true;
+          type = [ "nixfmt" ];
+        };
       };
-      oil = {
-        package = oil-nvim;
-        setup = "require('oil').setup()";
+      markdown.enable = true;
+      bash = {
+        enable = true;
+        extraDiagnostics.enable = true;
       };
-      kulala = {
-        package = kulala-nvim;
-        setup = # lua
-          ''
-            require('kulala').setup({
-              split_direction = "horizontal",
-              ft = {"http", "rest"},
-              global_keymaps = false,
-            })
-          '';
+      clang.enable = false;
+      css.enable = false;
+      html.enable = false;
+      json.enable = true;
+      sql.enable = false;
+      java.enable = false;
+      kotlin.enable = false;
+      ts.enable = false;
+      go.enable = true;
+      lua.enable = false;
+      python = {
+        enable = true;
+        dap.enable = true;
+        format = {
+          enable = true;
+          type = [ "ruff" ];
+        };
       };
-      git-worktree = {
-        package = git-worktree-nvim;
-        setup = # lua
-          ''
-            require('git-worktree').setup()
-            require('telescope').load_extension('git_worktree')
-          '';
+      typst.enable = false;
+      rust = {
+        enable = false;
+        extensions.crates-nvim.enable = false;
+      };
+      hcl.enable = true;
+      just.enable = false;
+      nim.enable = false;
+    };
+    lsp = {
+      enable = true;
+      formatOnSave = true;
+      lspkind.enable = true;
+      lightbulb.enable = true;
+      lspsaga.enable = false;
+      trouble.enable = true;
+      lspSignature.enable = true;
+      otter-nvim.enable = false;
+      nvim-docs-view.enable = false;
+      harper-ls.enable = false;
+    };
+    spellcheck = {
+      enable = true;
+      programmingWordlist.enable = false;
+    };
+    viAlias = true;
+    vimAlias = true;
+    visuals = {
+      nvim-scrollbar.enable = false;
+      nvim-web-devicons.enable = true;
+      nvim-cursorline.enable = true;
+      cinnamon-nvim.enable = false;
+      fidget-nvim.enable = true;
+      highlight-undo.enable = true;
+      indent-blankline.enable = true;
+    };
+    statusline = {
+      lualine = {
+        enable = true;
+        theme = "gruvbox";
       };
     };
+
+    theme = {
+      enable = true;
+      name = "gruvbox";
+      style = "dark";
+      transparent = false;
+    };
+
+    autopairs.nvim-autopairs.enable = true;
+
+    # nvf provides various autocomplete options. The tried and tested nvim-cmp is enabled in default package, because it does not trigger a build. We enable
+    # blink-cmp in maximal because it needs to build its rust fuzzy matcher library.
+    autocomplete = {
+      nvim-cmp.enable = true;
+      blink-cmp = {
+        enable = false;
+        setupOpts.signature.enabled = false;
+      };
+    };
+
+    snippets.luasnip.enable = true;
+
+    filetree = {
+      neo-tree = {
+        enable = true;
+      };
+    };
+
+    tabline.nvimBufferline.enable = true;
+
+    treesitter.context.enable = true;
+
+    binds = {
+      whichKey.enable = true;
+      cheatsheet.enable = true;
+    };
+
+    telescope.enable = true;
+
     git = {
+      enable = true;
       gitsigns.enable = true;
-      gitsigns.codeActions.enable = true;
+      gitsigns.codeActions.enable = true; # throws an annoying debug message
+      neogit.enable = true;
+    };
+
+    minimap = {
+      minimap-vim.enable = false;
+      codewindow.enable = false; # lighter, faster, and uses lua for configuration
+    };
+
+    dashboard = {
+      dashboard-nvim.enable = false;
+      alpha.enable = false;
+    };
+
+    notify = {
+      nvim-notify.enable = true;
+    };
+
+    projects = {
+      project-nvim.enable = true;
+    };
+    undoFile.enable = true;
+    utility = {
+      ccc.enable = false;
+      vim-wakatime.enable = false;
+      diffview-nvim.enable = true;
+      yanky-nvim.enable = false;
+      qmk-nvim.enable = false; # requires hardware specific options
+      icon-picker.enable = false;
+      surround.enable = true;
+      leetcode-nvim.enable = false;
+      multicursors.enable = false;
+      smart-splits.enable = false;
+      undotree.enable = false;
+      nvim-biscuits.enable = false;
+      oil-nvim = {
+        enable = true;
+        gitStatus.enable = true;
+      };
+
+      motion = {
+        hop.enable = true;
+        leap.enable = true;
+        precognition.enable = false;
+      };
+      images = {
+        image-nvim.enable = false;
+        img-clip.enable = false;
+      };
+    };
+
+    notes = {
+      neorg.enable = false;
+      orgmode.enable = false;
+      mind-nvim.enable = false;
+      todo-comments.enable = true;
+    };
+
+    terminal.toggleterm.enable = true;
+
+    ui = {
+      borders.enable = true;
+      noice = {
+        enable = true;
+        setupOpts = {
+          messages = {
+            enabled = true;
+            view = "notify";
+            view_error = "notify";
+            view_warn = "notify";
+            view_history = "messages";
+            view_search = "virtualtext";
+          };
+        };
+      };
+      colorizer.enable = true;
+      illuminate.enable = true;
+      breadcrumbs = {
+        enable = false;
+        navbuddy.enable = false;
+      };
+      smartcolumn = {
+        enable = true;
+        setupOpts.custom_colorcolumn = {
+          nix = "160";
+          ruby = "120";
+          java = "130";
+          go = [
+            "90"
+            "130"
+          ];
+        };
+      };
+      fastaction.enable = true;
+    };
+
+    assistant = {
+      chatgpt.enable = true;
+      copilot = {
+        enable = true;
+        cmp.enable = true;
+      };
+      codecompanion-nvim.enable = false;
+      avante-nvim.enable = false;
+    };
+
+    session = {
+      nvim-session-manager.enable = false;
+    };
+
+    gestures = {
+      gesture-nvim.enable = false;
+    };
+
+    comments = {
+      comment-nvim.enable = true;
+    };
+
+    # Custom formatter configuration - must come AFTER languages section
+    # to properly override the formatter arguments
+    formatter.conform-nvim.setupOpts = {
+      formatters.nixfmt = {
+        command = "${pkgs.nixfmt}/bin/nixfmt";
+        args = [
+          "--strict"
+          "--width"
+          "160"
+        ];
+      };
     };
     hideSearchHighlight = true;
+
     keymaps = [
       # Navigate splits
       {
@@ -92,7 +300,29 @@
         key = "<S-x>";
         mode = "n";
       }
-      # Move paragraphs
+      # Keep cursor in the same place when using 'J'
+      {
+        mode = "n";
+        action = "mzJ`z";
+        key = "J";
+      }
+      # Terminal mode mappings
+      {
+        action = "<C-\\><C-n>";
+        key = "<C-[>";
+        mode = "t";
+      }
+      {
+        action = "<C-\\><C-n><C-w>l<CR>";
+        key = "<S-l>";
+        mode = "t";
+      }
+      {
+        action = "<C-\\><C-n>:bprevious<CR>";
+        key = "<S-h>";
+        mode = "t";
+      }
+      #
       {
         action = ":m '>+1<CR>gv=gv";
         key = "J";
@@ -102,6 +332,17 @@
         action = ":m '<-2<CR>gv=gv";
         key = "K";
         mode = "n";
+      }
+      # Move paragraphs
+      {
+        action = ":m '>+1<CR>gv=gv";
+        key = "J";
+        mode = "v";
+      }
+      {
+        action = ":m '<-2<CR>gv=gv";
+        key = "K";
+        mode = "v";
       }
       # Stay in indent mode
       {
@@ -152,9 +393,25 @@
         action = ''"+Y'';
         key = "<leader>Y";
       }
+      # Terminal commands
+      {
+        action = "<cmd>:split term://glab mr create --assignee pblgomez --fill --push --no-editor --remove-source-branch --squash-before-merge --yes<CR>";
+        key = "<leader>tmc";
+        mode = "n";
+      }
+      {
+        action = "<cmd>lua require('toggleterm').exec('glab ci view', nil, nil, nil, 'float')<CR>";
+        key = "<leader>tv";
+        mode = "n";
+      }
+      {
+        action = "<cmd>lua require('toggleterm').exec('nix run nixpkgs#gh -- pr create', nil, nil, nil, 'float')<CR>";
+        key = "<leader>thc";
+        mode = "n";
+      }
       # Oil
       {
-        action = "<cmd>:Oil<CR>";
+        action = "<cmd>:lua require('oil').toggle_float()<CR>";
         key = "<leader>e";
         mode = "n";
       }
@@ -163,177 +420,30 @@
         key = "<leader>E";
         mode = "n";
       }
-      # Project_nvim
+      # Noice messages
       {
-        action = "<cmd>:Telescope projects<CR>";
-        key = "<leader>fp";
-        mode = "n";
-      }
-      # Neogit
-      {
-        action = "<cmd>Neogit<CR>";
-        key = "<leader>gs";
-        mode = "n";
-      }
-      {
-        action = "<cmd>Neogit commit<CR>";
-        key = "<leader>gc";
-        mode = "n";
-      }
-      {
-        action = "<cmd>Neogit pull<CR>";
-        key = "<leader>gp";
-        mode = "n";
-      }
-      {
-        action = "<cmd>Neogit push<CR>";
-        key = "<leader>gP";
-        mode = "n";
-      }
-      # Git worktree
-      {
-        action = "<cmd>:Telescope git_worktree git_worktrees<CR>";
-        key = "<leader>gw";
-        mode = "n";
-      }
-      {
-        action = "<cmd>:Telescope git_worktree create_git_worktree<CR>";
-        key = "<leader>gW";
-        mode = "n";
-      }
-
-      # Kulala (REST) commands
-      {
-        action = ":lua require('kulala').run()<CR>";
-        key = "<leader>Rs";
-        mode = "n";
-      }
-      {
-        action = ":lua require('kulala').replay()<CR>";
-        key = "<leader>Rr";
-        mode = "n";
-      }
-      # Terminal commands
-      {
-        action = "<cmd>:split term://glab mr create --assignee pblgomez --fill --push --no-editor --remove-source-branch --squash-before-merge --yes<CR>";
-        key = "<leader>tmc";
+        action = "<cmd>:Noice<CR>";
+        key = "<leader>nm";
         mode = "n";
       }
     ];
-    languages = {
-      enableDAP = true;
-      enableLSP = true;
-      enableTreesitter = true;
-      bash = {
-        enable = true;
-        extraDiagnostics.enable = true;
-        format.enable = true;
-      };
-      go = {
-        enable = true;
-        dap.enable = true;
-      };
-      nix = {
-        enable = true;
-        extraDiagnostics.enable = true;
-        format = {
-          type = "nixfmt";
-          enable = true;
-        };
-      };
-      python = {
-        enable = true;
-        dap.enable = true;
-      };
-      terraform.enable = true;
-    };
-    lsp = {
-      formatOnSave = true;
-    };
     luaConfigPost =
       # lua
       ''
-        vim.opt.colorcolumn = "80,160"
-        vim.opt.conceallevel= 1
-        vim.opt.foldlevel = 2
-        vim.opt.backup = false
         vim.opt.ignorecase = true
         vim.opt.scrolloff = 20
-        vim.opt.swapfile = false
         vim.opt.virtualedit = "block"
-        vim.api.nvim_create_autocmd({"BufRead","BufNewFile"},{pattern="*.http",command="setfiletype http",})
-
-        -- Git worktree indicator for lualine
-        local function get_git_worktree()
-          local handle = io.popen("git rev-parse --show-toplevel 2>/dev/null")
-          if handle then
-            local result = handle:read("*a")
-            handle:close()
-            if result and result ~= "" then
-              local worktree_name = result:match("([^/]+)%s*$")
-              if worktree_name then
-                return " " .. worktree_name
-              end
-            end
-          end
-          return ""
-        end
-
-        -- Add worktree component to lualine section b (after branch)
-        local lualine_config = require('lualine').get_config()
-        table.insert(lualine_config.sections.lualine_b, 2, {
-          get_git_worktree,
-          color = { fg = "#fabd2f", gui = "bold" },
-        })
-        require('lualine').setup(lualine_config)
       '';
     notes.obsidian = {
       enable = true;
       setupOpts.dir = "~/Notes";
+      setupOpts.legacy_commands = false;
     };
     options = {
       expandtab = true;
-      shiftwidth = 4;
-      tabstop = 4;
-      softtabstop = 4;
-    };
-    projects.project-nvim = {
-      enable = true;
-      setupOpts = {
-        manual_mode = false;
-        detection_methods = [ "pattern" ];
-      };
-    };
-    statusline.lualine.enable = true;
-    spellcheck = {
-      # TODO: Failing right now
-      enable = false;
-      languages = [ ];
-    };
-    tabline = {
-      nvimBufferline.enable = true;
-    };
-    theme = {
-      enable = true;
-      name = "gruvbox";
-      style = "dark";
-    };
-    telescope.enable = true;
-    ui = {
-      colorizer.enable = true;
-      noice.enable = true;
-      smartcolumn = {
-        enable = false;
-      };
-    };
-    utility.surround.enable = true;
-    undoFile.enable = true;
-    viAlias = true;
-    vimAlias = true;
-    visuals = {
-      nvim-web-devicons.enable = true;
-      fidget-nvim.enable = true;
-      indent-blankline.enable = true;
+      shiftwidth = 0;
+      tabstop = 2;
+      softtabstop = 2;
     };
   };
 }
